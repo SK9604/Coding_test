6b4eb12229e9a3226444f97a709b436ebdb540a5
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-06-21 20:36:01 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//freopen("input.txt","rt", stdin);
+	int n,age;
+	int max = 0,min=100;
+	cin>>n;
+	for(int i = 0; i<n; i++){
+		cin>>age;
+		if(age>=max) max = age;
+		if(age<=min) min = age;
+	}
+	cout<<max-min;
+	return 0;
+}
