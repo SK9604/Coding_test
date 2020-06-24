57c72b480aef73324cd74cc113980ad490177ff7
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-06-24 15:56:11 +0900

    Create baekjoon_11720.cpp

---
+++ baekjoon_11720.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,sum=0;
+	char s;
+	cin>>n;
+	for(int i = 0; i<n; i++){
+		cin>>s;
+		int t = s-'0';
+		sum+=t;
+	}
+	cout<<sum;
+	
+	return 0;
+}
