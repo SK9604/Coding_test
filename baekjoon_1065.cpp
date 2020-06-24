293b443625471df69d87738dbb4d20dcd87398d0
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-06-24 16:01:50 +0900

    Update baekjoon_1065.cpp

--- baekjoon_1065.cpp
+++ baekjoon_1065.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-int hansu(int n){
+int hansu(int n){  //한수 구하기(각 자리 숫자가 등차수열을 이루는 수
 	int o,t,h;
-	if(n<=99) return 1;
-	else if(n==1000) return 0;
-	else{
+	if(n<=99) return 1;       // 2자리 수 까지 모두 한수이다
+	else if(n==1000) return 0;//1000은 한수가 아니다
+	else{                     // 3자리 수 인 경우
 		while(n!=0){
 		o = n%10;
 		t = n/10%10;
 		h = n/100;
-		if(o-t == t-h) return 1; 
+		if(o-t == t-h) return 1; // 3-2자리 수의 차, 2-1자리 수의 차가 같은 경우= 한수
 		else return 0;
 		}
 	}
