7fb6906468af85b572e36272c07fd69358297c74
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-06-24 16:03:18 +0900

    Update baekjoon_11654.cpp

--- baekjoon_11654.cpp
+++ baekjoon_11654.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	char a;
+int main() {    // 아스키코드 구하기
+	char a; // 문자타입으로 받아오기
 	cin>>a;
-	cout<<(int)a;
+	cout<<(int)a; // int 타입으로 형 변환 시 아스키코드 출력 
 	return 0;
 }
