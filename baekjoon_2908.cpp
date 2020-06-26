3eb33610b1d5fd5a3e3a9369024f45c1c91e5b0a
SK <56914461+SK9604@users.noreply.github.com>
2020-06-26 21:44:52 +0900

    Update baekjoon_2908.cpp

--- baekjoon_2908.cpp
+++ baekjoon_2908.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int Digits(int n){
+int Digits(int n){ // 각 자리 수 별로 나눠 숫자 재조합
 	int one = n%10;
 	int ten = n/10%10;
 	int hundred = n/100;
-	return one*100+ten*10+hundred;
+	return one*100+ten*10+hundred; 
 }
-int main() {
+int main() {  // 두 개의 세자리 수를 받아 좌우 반전 한 후 더 큰 값을 출력
 	int a,b,at,bt;
 	cin>>a>>b;
 	at = Digits(a);
