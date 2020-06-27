b07fca46424b9b9a8a0eb67500ec4e74f317cc91
SK <56914461+SK9604@users.noreply.github.com>
2020-06-27 18:43:05 +0900

    Update baekjoon_2557.java

--- baekjoon_2557.java
+++ baekjoon_2557.java
@@ -1,4 +1,4 @@
-# 문제 URL: https://www.acmicpc.net/problem/2557
+// 문제 URL: https://www.acmicpc.net/problem/2557
 public class baekjoon_20353078 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
