bd64213432b4399a60e3bd74cdfe6abfa34aef80
SK <56914461+SK9604@users.noreply.github.com>
2020-06-27 18:43:45 +0900

    Update baekjoon_10718.java

--- baekjoon_10718.java
+++ baekjoon_10718.java
@@ -1,8 +1,9 @@
+// ë¬¸ì œ URL :https://www.acmicpc.net/problem/10718
 public class baekjoon_10718 {
 	public static void main(String[] args) {
 		int count = 0;
 		while(count<2) {
-			System.out.println("°­ÇÑÄ£±¸ ´ëÇÑÀ°±º");
+			System.out.println("Â°Â­Ã‡Ã‘Ã„Â£Â±Â¸ Â´Ã«Ã‡Ã‘Ã€Â°Â±Âº");
 			count++;
 		}
 	}
