52fc5083b8f025eaf1b33f39e0051a4bbc09f632
SK <56914461+SK9604@users.noreply.github.com>
2020-06-27 18:45:15 +0900

    Update baekjoon_10718.java

--- baekjoon_10718.java
+++ baekjoon_10718.java
@@ -3,7 +3,7 @@ public class baekjoon_10718 {
 	public static void main(String[] args) {
 		int count = 0;
 		while(count<2) {
-			System.out.println("°­ÇÑÄ£±¸ ´ëÇÑÀ°±º");
+			System.out.println("강한친구 대한육군");
 			count++;
 		}
 	}
