95fa2bffa33e1a3db4133691b045d5719b7d9777
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-06-28 17:58:52 +0900

    Create baekjoon_1712.cpp

---
+++ baekjoon_1712.cpp
@@ -0,0 +1,12 @@
+// ¹®Á¦ URL: https://www.acmicpc.net/problem/1712
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a,b,c;
+	cin>>a>>b>>c;
+	int x = a/(c-b);
+	if(c-b <= 0) cout<<-1;
+	else cout<<x+1;
+	return 0;
+}
