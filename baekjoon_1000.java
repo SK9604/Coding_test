c64a9929906814d2d1534123aea8c6b2d478b6ab
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-01 16:48:16 +0900

    Create baekjoon_1000.java

---
+++ baekjoon_1000.java
@@ -0,0 +1,11 @@
+// Question URL: https://www.acmicpc.net/source/1000
+import java.util.Scanner;
+public class baekjoon_1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		
+	}
+}
