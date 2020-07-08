e9dc180726b46c07b39690a7dd9f60f3ce2cbc63
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-08 15:23:30 +0900

    Create num_of_num(time limit).cpp
    자연수 N이 입력되면 1부터 N까지의 자연수를 종이에 적을 때 각 숫자는 몇 개 쓰였을까요?
    예를 들어 1부터 15까지는 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2, 1, 3, 1, 4, 1, 5으로
    총 21개가 쓰였음을 알 수 있습니다.
    자연수 N이 입력되면 1부터 N까지 각 숫자는 몇 개가 사용되었는지를 구하는 프로그램을 작
    성하세요. (시간 제한 1초)

---
+++ num_of_num(time limit).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, sum = 0, cnt = 1, d = 9, res = 0;
+	cin>>n;
+	while(sum+d<n){
+		res += (cnt*d);
+		sum+=d;
+		cnt++;
+		d *= 10;
+	}
+	res += ((n-sum) * cnt);
+	cout<<res;
+	return 0;
+}
