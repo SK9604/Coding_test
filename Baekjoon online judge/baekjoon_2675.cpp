134fb2f776bc1de9812a987314cbdb2a6774dbc5
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-10 17:27:21 +0900

    Create baekjoon_2675.cpp

---
+++ Baekjoon online judge/baekjoon_2675.cpp
@@ -0,0 +1,26 @@
+// Question URL: https://www.acmicpc.net/source/2675
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	int t,r;
+	char s;
+	string ss;
+	cin>>t;
+	for(int i = 0; i<t; i++){
+		cin>>r;
+		while(1){
+			cin.get(s);
+			if(s == '\n') break;
+			if(s==' ') continue;
+			for(int j = 0; j<r; j++){
+				ss+=s;
+			}
+		}
+		ss+="\n";
+	}
+	cout<<ss;
+	
+	return 0;
+}
