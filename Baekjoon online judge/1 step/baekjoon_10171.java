0cb5d99dba3c45ee2c9f1a2688a4ab10a1f46c46
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-10 17:37:59 +0900

    Create baekjoon_10171.java

---
+++ Baekjoon online judge/1 step/baekjoon_10171.java
@@ -0,0 +1,10 @@
+// Question URL: https://www.acmicpc.net/source/10171
+public class baekjoon_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
