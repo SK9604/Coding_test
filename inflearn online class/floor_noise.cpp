380296b641c11f1f10d3e16abe0a549e8982c0a4
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-11 16:51:54 +0900

    Create floor_noise.cpp

---
+++ inflearn online class/floor_noise.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,m,input,cnt=0,max=0;
+	cin>>n>>m;
+	for(int i = 0; i<n; i++){
+		cin>>input;
+		if(input>m) {
+			cnt++;
+			if(cnt>max) max = cnt;
+		}
+		else cnt=0;
+	}
+	if(max == 0) max = -1;
+	cout<<max;
+	return 0;
+}
