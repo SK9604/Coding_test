6df643df3fb40d2f5e5c669e241960e0dc2c8920
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-13 16:30:38 +0900

    Update max_value_of_temp.cpp
    온도의 최대값
    매일 아침 9시에 학교에서 측정한 온도가 어떤 정수의 수열로 주어졌을 때, 연속적인 며칠
    동안의 온도의 합이 가장 큰 값을 알아보고자 한다.
    예를 들어, 다음과 같이 10일 간의 온도가 주어졌을 때, 3 -2 -4 -9 0 3 7 13 8 -3 모든
    연속적인 이틀간의 온도의 합은 다음과 같다.
    이때, 온도의 합이 가장 큰 값은 21이다.
    매일 측정한 온도가 정수의 수열로 주어졌을 때, 연속적인 며칠 동안의 온도의 합이 가장 큰
    값을 계산하는 프로그램을 작성하시오.

--- inflearn online class/max_value_of_temp.cpp
+++ inflearn online class/max_value_of_temp.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	int i,n,k,max = -214000000,sum = 0;
 	cin>>n>>k;
-	int temp[n];
+	vector<int> temp(n);
 	for(i = 0; i<n; i++){
 		cin>>temp[i];
 	}
 	for(i = 0; i<k; i++){
 		sum+=temp[i];
-		max = sum;
 	}
+	max = sum;
 	for(i = k; i<n; i++){
 		sum+=temp[i]-temp[i-k];
 		if(sum>max) max = sum;
