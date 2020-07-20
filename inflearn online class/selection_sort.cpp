f064bfb9f813739944945cba4ccb8564080a2e6a
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-20 18:51:52 +0900

    Create selection_sort.cpp
    선택정렬
    N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
    정렬하는 방법은 선택정렬입니다.

---
+++ inflearn online class/selection_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, min = 214000000;
+	cin>>n;
+	vector <int> input(n);
+	for(int i = 0; i<n; i++){
+		cin>>input[i];
+	}
+	for(int i = 0; i<input.size(); i++){
+		int idx = i;
+		for(int j = i+1; j<input.size(); j++){
+			if(input[j]<input[i]) idx = j;
+		}
+		int tmp = input[i];
+		input[i] = input[idx];
+		input[idx] = tmp;
+	}
+	for(int i = 0; i<input.size(); i++){
+		cout<<input[i]<<' ';
+	}
+	return 0;
+}
