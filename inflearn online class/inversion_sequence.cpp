d066842167811d31f91d14d9448978b0b0b06d81
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-07-25 16:46:32 +0900

    Create inversion_sequence.cpp
    Inversion Sequence
    1부터 n까지의 수를 한 번씩만 사용하여 이루어진 수열이 있을 때, 1부터 n까지 각각의 수
    앞에 놓여 있는 자신보다 큰 수들의 개수를 수열로 표현한 것을 Inversion Sequence라 한다.
    예를 들어 다음과 같은 수열의 경우
    4 8 6 2 5 1 3 7
    1앞에 놓인 1보다 큰 수는 4, 8, 6, 2, 5. 이렇게 5개이고,
    2앞에 놓인 2보다 큰 수는 4, 8, 6. 이렇게 3개,
    3앞에 놓인 3보다 큰 수는 4, 8, 6, 5 이렇게 4개......
    따라서 4 8 6 2 5 1 3 7의 inversion sequence는 5 3 4 0 2 1 1 0 이 된다.
    n과 1부터 n까지의 수를 사용하여 이루어진 수열의 inversion sequence가 주어졌을 때, 원래
    의 수열을 출력하는 프로그램을 작성하세요.

---
+++ inflearn online class/inversion_sequence.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n,cnt;
+	cin>>n;
+	vector <int> is(n);
+	vector <int> res(n);
+	for(int i = 0; i<n; i++){
+		cin>>is[i];
+	}
+	for(int i = 1; i<=n; i++){
+		cnt = is[i-1]+1;
+		for(int j = 0; j<n; j++){
+			if(res[j] == 0){
+				cnt--;
+			}
+			if(cnt == 0&&res[j]==0){
+				res[j] = i;
+			}
+			else if(cnt == 0&&res[j]!=0){
+				continue;
+			}
+			
+		}
+	}
+	/*
+	원래 수열에서 inversion sequence로 변경하는 코드 
+	for(int i = 1; i<=n; i++){
+		cnt=0;
+		for(int j = 0; j<n; j++){
+			if(i < is[j]){
+				cnt++;
+			}
+			else if(i == is[j]){
+				res[i-1] = cnt;
+				break;
+			}
+			else continue;
+		}
+	}
+	*/
+	for(int i = 0; i<n; i++){
+		cout<<res[i]<<' ';
+	}
+	return 0;
+}
