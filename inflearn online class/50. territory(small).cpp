65759b9c8cce9d729b6f81dc3d6b2e84c2d23ea6
SK9604 <56914461+SK9604@users.noreply.github.com>
2020-08-03 15:12:05 +0900

    Create 50. territory(small).cpp
    영지(territory) 선택 : (small)
    세종대왕은 현수에게 현수가 다스릴 수 있는 영지를 하사하기로 했다. 전체 땅은 사각형으로 표
    시된다. 그 사각형의 땅 중에서 세종대왕이 현수가 다스릴 수 있는 땅의 크기(세로의 길이와 가
    로의 길이)를 정해주면 전체 땅 중에서 그 크기의 땅의 위치를 현수가 정하면 되는 것이다.
    전체 땅은 사각형의 모양의 격자로 되어 있으며, 그 사각형 땅 안에는 많은 오렌지 나무가 심
    겨져 있다. 현수는 오렌지를 무척 좋아하여 오렌지 나무가 가장 많이 포함되는 지역을 선택하
    고 싶어 한다. 현수가 얻을 수 있는 영지의 오렌지 나무 최대 개수를 출력하는 프로그램을 작
    성하세요. 다음과 같은 땅의 정보가 주어지고, 현수가 하사받을 크기가, 가로 2, 세로 3의 크
    기이면 가장 많은 오렌지 나무가 있는 영지는 총 오렌지 나무의 개수가 16인 3행 4열부터 시
    작하는 구역이다.

---
+++ inflearn online class/50. territory(small).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int h,w,hh,hw,sum,max=-2147000000;
+	cin>>h>>w;
+	vector <vector <int> > tree (h, vector <int> (w));
+	for(int i =0; i<h; i++){
+		for(int j = 0; j<w; j++){
+			cin>>tree[i][j];
+		}
+	}
+	cin>>hh>>hw;
+	for(int i = 0; i<h-hh; i++){
+		for(int j = 0; j<w-hw; j++){
+			sum = 0;
+			for(int x = i; x<i+hh; x++){
+				for(int y = j; y<j+hw; y++){
+					sum+=tree[x][y];
+				}
+			}
+			if(max<sum){
+				max = sum;
+			}
+		}
+	}
+	cout<<max;
+	return 0;
+}
