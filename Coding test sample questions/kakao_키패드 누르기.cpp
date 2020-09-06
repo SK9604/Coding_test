846a0dd2f2c9f58c739db634d237fd57cb4a76cd
SK <56914461+SK9604@users.noreply.github.com>
2020-09-06 17:51:34 +0900

    Rename Kakao_키패드 누르기.cpp to kakao_키패드 누르기.cpp

---
+++ Coding test sample questions/kakao_키패드 누르기.cpp
@@ -0,0 +1,67 @@
+﻿#include "stdafx.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<int> numbers, string hand) {
+	string answer = "";
+	int lx = 0, rx = 2, ly = 3, ry = 3;
+	int x, y;
+	for (int i = 0; i<numbers.size(); i++) {
+		if (numbers[i] % 3 == 1) {
+			answer += "L";
+			lx = 0;
+			ly = numbers[i] / 3;
+		}
+		else if (numbers[i] != 0 && numbers[i] % 3 == 0) {
+			answer += "R";
+			rx = 2;
+			ry = (numbers[i] / 3) - 1;
+		}
+		else if (numbers[i] % 3 == 2 || numbers[i] == 0) {
+			if (numbers[i] == 0) {
+				x = 1;
+				y = 3;
+			}
+			else {
+				x = 1;
+				y = numbers[i] / 3;
+			}
+
+			int d_r = abs(ry - y) + abs(rx - x);
+			int d_l = abs(ly - y) + abs(lx - x);
+
+			if (d_r < d_l) {
+				answer += "R";
+				rx = x;
+				ry = y;
+			}
+			else if (d_r > d_l) {
+				answer += "L";
+				lx = x;
+				ly = y;
+			}
+			else {
+				if (hand == "right") {
+					answer += "R";
+					rx = x;
+					ry = y;
+				}
+				else {
+					answer += "L";
+					lx = x;
+					ly = y;
+				}
+			}
+		}
+	}
+	return answer;
+}
+
+int main() {
+	vector <int> numbers = { 7,0,8,2,8,3,1,5,7,6,2 };
+	string hand = "left";
+	cout << solution(numbers,hand);
+}
