d9da71d959a041b037d1632124be667bdb86bc62
SK9604 <tjsrud428@gmail.com>
2020-09-25 13:04:45 +0900

    Create programers_level1_1.cpp

---
+++ Programmers/programers_level1_1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    int min = 2147000000;
+    for(int i = 0; i<arr.size(); i++){
+        if(arr[i]<min) min = arr[i];
+    }
+    for(int i = 0; i<arr.size(); i++){
+        if(arr[i]!=min) answer.push_back(arr[i]);
+    }
+    if(answer.size() == 0) answer.push_back(-1);
+    return answer;
+}
