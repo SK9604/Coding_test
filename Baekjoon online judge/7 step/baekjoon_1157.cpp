d71b17849d8498dc8c6942fb17c33e605a530a8e
SK9604 <tjsrud428@gmail.com>
2020-09-26 15:23:40 +0900

    Create baekjoon_1157.cpp

---
+++ Baekjoon online judge/7 step/baekjoon_1157.cpp
@@ -0,0 +1,28 @@
+// Question URL: https://www.acmicpc.net/source/1157
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	char s,out;
+	int max = -1;
+	int alphabet[26]={0};
+	while(1){
+		cin.get(s);
+		if(s=='\n')break;
+		if((int)s>=97 and (int)s<=122)
+			s -=32;
+		alphabet[(int)s-'A']++;		
+	}
+	for(int i = 0; i<26; i++){
+		int temp;
+		if(alphabet[i]>max) {
+			max = alphabet[i];
+			temp = i;
+			out = i+'A';
+		}
+		if(alphabet[i]==max and temp != i) out = '?';  
+	}
+	cout<<out;
+	return 0;
+}
