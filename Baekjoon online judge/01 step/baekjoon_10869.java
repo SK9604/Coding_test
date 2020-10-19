062908511200f18251e52a0537e5bc7957873241
SK9604 <tjsrud428@gmail.com>
2020-10-20 00:08:39 +0900

    Create baekjoon_10869.java

---
+++ Baekjoon online judge/01 step/baekjoon_10869.java
@@ -0,0 +1,15 @@
+// Question URL: https://www.acmicpc.net/source/10869
+import java.util.Scanner;
+public class baekjoon_10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	
+	}
+}
