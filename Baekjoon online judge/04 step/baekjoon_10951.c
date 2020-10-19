7788d8c28d9b45bab83151d364e81b70181dbde5
SK9604 <tjsrud428@gmail.com>
2020-10-20 00:07:12 +0900

    Create baekjoon_10951.c

---
+++ Baekjoon online judge/04 step/baekjoon_10951.c
@@ -0,0 +1,16 @@
+//Qusetion URL : https://www.acmicpc.net/problem/10951
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a, b;
+	while (cin >> a >> b) {
+		cout << a + b << '\n';
+	}
+
+    return 0;
+}
