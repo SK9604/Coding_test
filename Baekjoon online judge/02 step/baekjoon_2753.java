853e331ad0f0538327cbe9857092f3f18d9c16ce
SK9604 <tjsrud428@gmail.com>
2020-10-20 00:06:47 +0900

    Create baekjoon_2753.java

---
+++ Baekjoon online judge/02 step/baekjoon_2753.java
@@ -0,0 +1,14 @@
+// Question URL: https://www.acmicpc.net/source/2753
+import java.util.Scanner;
+public class baekjoon_2753 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		if(A%4==0&&A%100!=0||A%400==0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+		
+	}
+}
