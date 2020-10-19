73cf76c3ae9652c12d3812d1e3a111a1ab444a01
SK9604 <tjsrud428@gmail.com>
2020-10-20 00:08:00 +0900

    Create baekjoon_1085.cpp

---
+++ Baekjoon online judge/09 step/baekjoon_1085.cpp
@@ -0,0 +1,15 @@
+//Question URL : https://www.acmicpc.net/problem/1085
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x, y, w, h, min = 1000;
+	cin>>x>>y>>w>>h;
+	if(w-x<min) min = w-x;
+	if(h-y<min) min = h-y;
+	if(x<min) min = x;
+	if(y<min) min = y;
+	cout<<min;
+	return 0;
+}
+
